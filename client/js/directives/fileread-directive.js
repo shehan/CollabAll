c5d86ef98feb35497981947df73e54c1e4b778cf
shehan peruma <shehan.peruma@gmail.com>
2017-02-22 14:10:28 -0500

    added

---
+++ client/js/directives/fileread-directive.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('FileReadDirective', [])
+
+    .directive("fileread", [function () {
+        return {
+            scope: {
+                fileread: "=",
+                format: "="
+            },
+            link: function (scope, element) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+
+                    reader.onloadend = function() {
+                        //First, check that the format is pdf, docx, or doc
+                        var filename = element[0].value.replace(/.*[\/\\]/, '');
+                        filename = filename.split('.');
+                        var format = filename[filename.length-1];
+                        if(format !== 'pdf' && //TODO consider refactorign so this is reusable
+                            format !== 'doc' &&
+                            format !== 'docx'){
+                            scope.$apply(function() {
+                                scope.format = false;
+                            });
+                            console.log('Invalid Format');
+                            return;
+                        }
+
+
+                        var dataUrl = reader.result;
+                        var base64 = dataUrl.split(',')[1];
+                        scope.$apply(function() {
+                            scope.fileread = base64;
+                            scope.format = format;
+                        });
+                    };
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        };
+    }]);
