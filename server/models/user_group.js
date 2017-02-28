913e2a537ed9adc56fe4e5501b0e3fd2659f8eb7
shehan peruma <shehan.peruma@gmail.com>
2017-02-28 00:24:12 -0500

    updated model to associate with Group and User models

--- server/models/user_group.js
+++ server/models/user_group.js
@@ -3,13 +3,21 @@
     module.exports = function (sequelize, DataTypes) {
         var user_group;
         user_group = sequelize.define("user_group", {
-            ID: {
-                type: DataTypes.INTEGER,
-                primaryKey: true,
-                autoIncrement: true
+                ID: {
+                    type: DataTypes.INTEGER,
+                    primaryKey: true,
+                    autoIncrement: true
+                },
+                IsActive: DataTypes.BOOLEAN
             },
-            IsActive: DataTypes.BOOLEAN
-        });
+            {
+                classMethods: {
+                    associate: function (models) {
+                        user_group.belongsTo(models.user);
+                        user_group.belongsTo(models.group);
+                    }
+                }
+            });
         return user_group;
     };
 }());
