cc4d84240dfdb7116b7b66f5d1b6091a1a8425eb
shehan peruma <shehan.peruma@gmail.com>
2017-03-03 16:28:51 -0500

    removed socket code from home controller  and added to inside.home controller

--- client/js/home-controller.js
+++ client/js/home-controller.js
@@ -5,15 +5,6 @@
 
         .controller('homeController', ['$scope', '$state', '$http','AuthService','$window', function($scope, $state, $http, AuthService,$window) {
 
-            var user = 'userid=' + $scope.userID;
-            var socket = io.connect({query: user});
-            socket.on('notification message', function (msg) {
-                alert(msg);
-                $scope.hasNotifications = true;
-                $scope.$apply();
-            });
-            console.log(socket);
-
             $scope.title = "CollabAll";
             $scope.user =  AuthService.authenticatedUser();
 
