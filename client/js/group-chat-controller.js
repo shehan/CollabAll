8de555f5d93edfe0255aa9c7510617831e6d0158
shehan peruma <shehan.peruma@gmail.com>
2017-03-15 23:42:45 -0400

    updated meetnow screen

--- client/js/group-chat-controller.js
+++ client/js/group-chat-controller.js
@@ -3,21 +3,22 @@
 
     angular.module('GroupChatControllerModule', [])
 
-        .controller('groupChatController', ['$scope', 'AuthService', '$state', '$http','$stateParams','$window',
-            function ($scope, AuthService, $state, $http, $stateParams,$window) {
+        .controller('groupChatController', ['$scope', 'AuthService', '$state', '$http', '$stateParams', '$window',
+            function ($scope, AuthService, $state, $http, $stateParams, $window) {
 
                 $scope.title = "CollabAll - Group Chat";
                 $scope.contactAuthor = AuthService.authenticatedUser().FirstName + " " + AuthService.authenticatedUser().LastName;
                 $scope.userID = AuthService.authenticatedUser().ID;
                 $scope.groupID = $stateParams.groupID;
-                $scope.group=[];
-                $scope.groupMembers=[];
-                $scope.stream='';
-                $scope.prevAction='';
-                $scope.deviceOrientation={};
-                $scope.deviceOrientation.alpha='';
-                $scope.deviceOrientation.beta='';
-                $scope.deviceOrientation.gamma='';
+                $scope.group = [];
+                $scope.groupMembers = [];
+                $scope.groupCards = [];
+                $scope.stream = '';
+                $scope.prevAction = '';
+                $scope.deviceOrientation = {};
+                $scope.deviceOrientation.alpha = '';
+                $scope.deviceOrientation.beta = '';
+                $scope.deviceOrientation.gamma = '';
 
 
                 document.getElementById("overlayScreen").style.width = "100%";
@@ -28,35 +29,40 @@
                         $scope.groupMembers = response.data.users;
                         $scope.groupMembers.sort(compare);
 
-                        $http.get('services/group/get-group-by-id', {params: {GroupId: $scope.groupID}})
-                            .then(function(response){
-                                $scope.group = response.data.group;
-                                document.getElementById("overlayScreen").style.width = "0%";
-                                document.getElementById("overlayScreen").style.height = "0%";
+                        $http.get('services/card/get-cards-for-group', {params: {GroupId: $scope.groupID}})
+                            .then(function (response) {
+                                $scope.groupCards = response.data.cards;
+
+                                $http.get('services/group/get-group-by-id', {params: {GroupId: $scope.groupID}})
+                                    .then(function (response) {
+                                        $scope.group = response.data.group;
+                                        document.getElementById("overlayScreen").style.width = "0%";
+                                        document.getElementById("overlayScreen").style.height = "0%";
+                                    });
                             });
                     });
 
 
                 var socket = io.connect({query: $scope.userID});
 
-                socket.on('connect', function(msg){
-                    socket.emit('join',$scope.userID);
+                socket.on('connect', function (msg) {
+                    socket.emit('join', $scope.userID);
                     console.log("client joining server");
                 });
 
-                socket.emit("subscribe", { group: "1" });
+                socket.emit("subscribe", {group: "1"});
 
                 //     socket.emit("unsubscribe", { group: "1" });
 
-                socket.on("disconnect", function(){
+                socket.on("disconnect", function () {
                     console.log("client disconnected from server");
                 });
 
-                socket.on('interjection', function(data) {
+                socket.on('interjection', function (data) {
                     console.log('Incoming interjection:', data);
                 });
 
-                socket.on('tilt', function(data) {
+                socket.on('tilt', function (data) {
                     $scope.stream = $scope.stream + data + "\n";
                     $scope.$apply();
                 });
@@ -66,18 +72,18 @@
                 if ($window.DeviceMotionEvent) {
                     $window.addEventListener("devicemotion", motion, true);
                 }
-                else{
+                else {
                     alert("DeviceMotionEvent NOT supported");
                 }
 
                 if ($window.DeviceOrientationEvent) {
                     $window.addEventListener("deviceorientation", orientation, false);
                 }
-                else{
+                else {
                     alert("DeviceOrientation NOT supported");
                 }
 
-                function motion(event){
+                function motion(event) {
                     $scope.deviceMotion = "Accelerometer: "
                         + event.accelerationIncludingGravity.x + ", "
                         + event.accelerationIncludingGravity.y + ", "
@@ -85,13 +91,19 @@
                     $scope.$apply();
                 }
 
-                function orientation(event){
-                    $scope.deviceOrientation.alpha=event.alpha;
-                    $scope.deviceOrientation.beta=event.beta;
-                    $scope.deviceOrientation.gamma=event.gamma;
+                function orientation(event) {
+                    $scope.deviceOrientation.alpha = event.alpha;
+                    $scope.deviceOrientation.beta = event.beta;
+                    $scope.deviceOrientation.gamma = event.gamma;
 
-                    if (event.beta >  90) { $scope.deviceOrientation.beta =  90};
-                    if (event.beta < -90) { $scope.deviceOrientation.beta = -90};
+                    if (event.beta > 90) {
+                        $scope.deviceOrientation.beta = 90
+                    }
+                    ;
+                    if (event.beta < -90) {
+                        $scope.deviceOrientation.beta = -90
+                    }
+                    ;
 
                     $scope.deviceOrientation.beta += 90;
                     $scope.deviceOrientation.gamma += 90;
@@ -100,22 +112,21 @@
 
                 }
 
-                var pos='';
-                setInterval(function(){
-                    if($scope.deviceOrientation.beta >= 40 && $scope.deviceOrientation.beta<=66)
+                var pos = '';
+                setInterval(function () {
+                    if ($scope.deviceOrientation.beta >= 40 && $scope.deviceOrientation.beta <= 66)
                         pos = "DOWN";
-                    if($scope.deviceOrientation.beta >= 150 && $scope.deviceOrientation.beta<=170)
+                    if ($scope.deviceOrientation.beta >= 150 && $scope.deviceOrientation.beta <= 170)
                         pos = "UP";
-                    if($scope.deviceOrientation.gamma >= 30 && $scope.deviceOrientation.gamma<=80)
+                    if ($scope.deviceOrientation.gamma >= 30 && $scope.deviceOrientation.gamma <= 80)
                         pos = "LEFT";
-                    if($scope.deviceOrientation.gamma >= 115 && $scope.deviceOrientation.gamma<=150)
+                    if ($scope.deviceOrientation.gamma >= 115 && $scope.deviceOrientation.gamma <= 150)
                         pos = "RIGHT";
 
-                    if (pos != ''){
-                        if(pos != $scope.prevAction)
-                        {
+                    if (pos != '') {
+                        if (pos != $scope.prevAction) {
                             $scope.prevAction = pos;
-                            socket.emit("deviceTilt", { deviceOrientation:  pos });
+                            socket.emit("deviceTilt", {deviceOrientation: pos});
                             $window.navigator.vibrate(200);
                         }
                     }
