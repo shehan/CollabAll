899fa4392612aaace975a384d0063625184fc065
shehan peruma <shehan.peruma@gmail.com>
2017-03-21 17:47:57 -0400

    use mysql hosted on the cloud

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,11 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": "bd8fdea224eabf",
+    "password": "94b5a7d3",
+    "database": "database_dev",
+    "host": "us-cdbr-azure-east2-d.cloudapp.net",
 	"storage": "database_dev.sqlite",
-    "dialect": "sqlite"
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
