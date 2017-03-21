cc38d61a53c2b7120963f922888f1966d7cd7802
shehan peruma <shehan.peruma@gmail.com>
2017-03-21 17:55:08 -0400

    use mysql hosted on the cloud

--- server/config/config.js
+++ server/config/config.js
@@ -8,19 +8,19 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-	"storage": "database_test.sqlite",
-    "dialect": "sqlite"
+    "username": "bd8fdea224eabf",
+    "password": "94b5a7d3",
+    "database": "database_dev",
+    "host": "us-cdbr-azure-east2-d.cloudapp.net",
+    "storage": "database_dev.sqlite",
+    "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-	"storage": "database_prod.sqlite",
-    "dialect": "sqlite"
+    "username": "bd8fdea224eabf",
+    "password": "94b5a7d3",
+    "database": "database_dev",
+    "host": "us-cdbr-azure-east2-d.cloudapp.net",
+    "storage": "database_dev.sqlite",
+    "dialect": "mysql"
   }
 };
